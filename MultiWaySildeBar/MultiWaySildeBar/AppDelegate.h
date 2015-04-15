a6ba34399ca2a93b6deded0c14cee2cef644bc18
DavidLam <david.lam@zensis.com>
2015-04-15 15:49:16 +0800

    *init project

---
+++ MultiWaySildeBar/MultiWaySildeBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultiWaySildeBar
+//
+//  Created by Zensis on 15/4/15.
+//  Copyright (c) 2015 David. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MultiWaySildeBar/MultiWaySildeBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MultiWaySildeBar
+//
+//  Created by Zensis on 15/4/15.
+//  Copyright (c) 2015 David. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
